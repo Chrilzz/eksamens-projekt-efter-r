6af2142d121f217d950c8b723548546e61cc3946
Chrilzz <christiansc2000@gmail.com>
2021-11-29 00:41:44 +0100

    Fik den til at gemme en bruger, til en JSON fil endelig

--- opretbruger.js
+++ opretbruger.js
@@ -1,10 +1,21 @@
-const opretBruger = e => {
-    
-    let oprettelsesdata = {
-        Brugernavn: document.getElementById("Brugernavn").value,
-        Adgangskode: document.getElementById("Adgangskode").value
+ //
+document.getElementById("opret-bruger").addEventListener("click", (e) => { 
+    e.preventDefault() 
+    let brugernavnværdi = document.getElementById("bruger-Navn").value
+    let adgangskodeværdi = document.getElementById("adgangs-Kode").value
+    let nykonto = {
+        brugernavn: brugernavnværdi,
+        adgangskode: adgangskodeværdi
     }
-    localStorage.setItem('oprettelsesdata', JSON.stringify(oprettelsesdata))
-    console.log(localStorage.getItem('oprettelsesdata'))
-    e.preventDefault();
-}
+console.log(nykonto)
+  
+fetch('http://localhost:8080/konto', {
+    method: "POST",
+    headers: {
+        'content-Type': 'application/json'
+    },
+    body: JSON.stringify(nykonto)
+}).then(response => response.json())
+.then(location.href = "/Logind.html")
+
+ })
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-/* Ved brug af express.js opret en server, som lytter på port 8080, og som console loggerbeskeden: “Server lytter på port 8080”
+//Ved brug af express.js opret en server, som lytter på port 8080, og som console loggerbeskeden: “Server lytter på port 8080”
 const express = require("express");
 const app = express();
-
-const BrugerController = require("./Controllers/Brugercontroller")
+const fs = require('fs')
 
 const port = 8080
 app.listen(port, () => {
@@ -15,11 +14,24 @@ app.get("/", (req, res) => {
 
 //linker mit css funktionalitet til min server så mine designs virker og knapperne virker.
 //https://expressjs.com/en/starter/static-files.html
-app.use(express.static("./frontend"));
+app.use(express.static('./'))
 
 //for at det er muligt at lave input til serveren i tekststreng
-app.use(express.json)
+app.use(express.json())
 
-app.use("/brugere", BrugerController)
+//Gemmer bruger til JSONFIL
+//_______________________________________
 
-*/
+app.post("/konto",(req, res) => { 
+
+    let data = req.body 
+
+      res.status(200).json("modtaget status"); 
+console.log(data)
+console.log(typeof data)
+
+fs.writeFileSync('brugerData.json',JSON.stringify(req.body))
+  } 
+); 
+
+let brugerdatasa = JSON.parse(fs.readFileSync('brugerData.json'))
